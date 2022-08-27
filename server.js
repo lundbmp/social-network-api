68d200acaa96f5c3e20934d4003ac250d1bc3587
lundbmp <m.lundberg008@gmail.com>
2022-08-26 20:07:36 -0500

    added mongoose connection TODO: models

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,7 +8,19 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+// database connection
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/socialNetworkDB",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (error) => {
+    !error
+      ? console.log("Connected to database...")
+      : console.log("Error connecting to database: " + error);
+  }
+);
 
 // server connection
 app.listen(PORT, () => console.log(`Connected on port: ${PORT}...`));
